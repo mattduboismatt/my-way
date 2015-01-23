fdea18cdb88b989dc6fe6c63ea6046de9cc6ac94
Matt DuBois <mattdubois2@gmail.com>
2015-01-23 17:29:35 -0600

    add transit details for stops / lines / headsigns

--- lib/modules/google_maps/google_maps_runner.rb
+++ lib/modules/google_maps/google_maps_runner.rb
@@ -8,12 +8,13 @@ require_relative './google_maps.rb'
 # it is posisble to white/black list which requests to make.. TBD
 # first request sets up the trip and the next 3 add routes to that trip
 
-a = "2756 N. Pine Grove, Chicago, IL"
-b = "409 W. North Ave Chicago IL"
+b = "2756 N. Pine Grove, Chicago, IL"
+a = "505 Fair Ave., Elmhurst IL"
 init_mode = "driving"
 other_modes = ["walking","bicycling","transit"]
 
 req = GoogleMaps.build_uri(a,b,init_mode)
+puts req
 res = Net::HTTP.get(req)
 trip = GoogleMaps.generate_trip(res)
 
@@ -23,14 +24,14 @@ other_modes.each do |m|
   GoogleMaps.generate_trip(res, trip)
 end
 
-# i = 1
+i = 1
 
-# trip.routes.each do |r|
-#   p "Route"+i.to_s
-#   p "Route travel mode: "+r.travel_mode
-#   r.steps.each do |s|
-#     p s.travel_mode
-#   end
-#   p "=================="
-#   i += 1
-# end
+trip.routes.each do |r|
+  p "Route"+i.to_s
+  p "Route travel mode: "+r.travel_mode
+  r.steps.each do |s|
+    puts s.inspect
+  end
+  p "=================="
+  i += 1
+end
