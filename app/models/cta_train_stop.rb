6303248b376ab41acf7a1bf44705811e88fa9a07
Nirag Amin <nirag.amin@gmail.com>
2015-01-23 13:23:49 -0600

    Adding iteration to find stations method

--- app/models/cta_train_stop.rb
+++ app/models/cta_train_stop.rb
@@ -1,7 +1,11 @@
+require pry
 class CtaTrainStop < ActiveRecord::Base
-  def self.find_nearest_station(lat, lng, dist)
-    distance = dist
-    CtaTrainStop.where("lat < ? AND lng < ? AND lat > ? AND lng > ? ", lat+distance, lng + distance,lat-distance, lng - distance)
+  def self.find_nearest_stations(lat, lng)
+    distance = 1.0005
+    while CtaTrainStop.where("lat < ? AND lng < ? AND lat > ? AND lng > ? ", lat+distance, lng + distance,lat-distance, lng - distance)
+      binding.pry
+      distance += 0.0005
+    end
   end
 
 end
