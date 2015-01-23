82f49c92c85f66e17c297bfafb0f916a23ddb0b3
Nirag Amin <nirag.amin@gmail.com>
2015-01-23 12:52:57 -0600

    Adding nearest station logic

--- app/models/cta_train_stop.rb
+++ app/models/cta_train_stop.rb
@@ -1,2 +1,7 @@
 class CtaTrainStop < ActiveRecord::Base
+  def self.find_nearest_station(lat, lng, dist)
+    distance = dist
+    CtaTrainStop.where("lat < ? AND lng < ? AND lat > ? AND lng > ? ", lat+distance, lng + distance,lat-distance, lng - distance)
+  end
+
 end
