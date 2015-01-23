e71a05cf964d5c110b8a3114fafc56cc885a1d8a
Nirag Amin <nirag.amin@gmail.com>
2015-01-23 10:43:24 -0600

    Adding cta trail model and migration

---
+++ app/models/cta_train_stop.rb
@@ -0,0 +1,2 @@
+class CtaTrainStop < ActiveRecord::Base
+end
---
+++ db/migrate/20150123163508_create_cta_train_stops.rb
@@ -0,0 +1,24 @@
+class CreateCtaTrainStops < ActiveRecord::Migration
+  def change
+    create_table :cta_train_stops do |t|
+      t.integer :stop_id
+      t.string :direction_id
+      t.string :stop_name
+      t.float :lng
+      t.float :lat
+      t.string :station_name
+      t.string :station_descriptive_name
+      t.string :parent_stop_id
+      t.boolean :ada
+      t.boolean :red
+      t.boolean :brn
+      t.boolean :g
+      t.boolean :p
+      t.boolean :pexp
+      t.boolean :pink
+      t.boolean :org
+
+      t.timestamps null: false
+    end
+  end
+end
