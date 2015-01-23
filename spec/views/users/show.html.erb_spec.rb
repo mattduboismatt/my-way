2b7d32aa9a0a9155b00064715458468a1d7649ef
Matt DuBois <mattdubois2@gmail.com>
2015-01-22 19:20:04 -0600

    user scaffold yippeee we're done

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", :type => :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :name => "Name",
+      :email => "Email",
+      :password_digest => "Password Digest",
+      :age => 1,
+      :username => "Username"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Email/)
+    expect(rendered).to match(/Password Digest/)
+    expect(rendered).to match(/1/)
+    expect(rendered).to match(/Username/)
+  end
+end
