87783b90f5cfe167d710fd6bcdeb0b9bda4dc740
Nirag Amin <nirag.amin@gmail.com>
2015-01-23 15:05:19 -0600

    adding exponential increase to radius to find stations

--- app/models/cta_train_stop.rb
+++ app/models/cta_train_stop.rb
@@ -1,8 +1,15 @@
 require 'pry'
 class CtaTrainStop < ActiveRecord::Base
+  scope :radius, ->(lat, lng, distance) {where("lat < ? AND lng < ? AND lat > ? AND lng > ? ", lat+distance, lng + distance,lat-distance, lng - distance)}
+
   def self.find_stations(lat, lng)
-    distance = 1.0005
-    CtaTrainStop.where("lat < ? AND lng < ? AND lat > ? AND lng > ? ", lat+distance, lng + distance,lat-distance, lng - distance)
+    distance = 0.0005
+    count = 0
+    while CtaTrainStop.radius(lat, lng, distance).count < 4
+      distance += 0.0005*Math.exp(count)
+      count += 1
+    end
+    CtaTrainStop.radius(lat, lng, distance)
   end
 
   def self.closest_station(lat,lng)
