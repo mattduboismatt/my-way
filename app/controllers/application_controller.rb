568bb08ff1f8b51482f80191dd0bc4dd8bdc4c16
Matt DuBois <mattdubois2@gmail.com>
2015-01-23 11:32:46 -0600

    trip form added to landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
     @_current_user ||= session[:user_id]
     User.find_by(id: session[:user_id])
   end
-
   helper_method :current_user
 
   def logged_in?
