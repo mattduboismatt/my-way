4c8e2f8c8fe9803b0f78fe4810f853e4a45da726
Nirag Amin <nirag.amin@gmail.com>
2015-01-24 15:45:39 -0600

    Adding method to find closest divvy station

--- app/models/divvy.rb
+++ app/models/divvy.rb
@@ -1,8 +1,34 @@
 class Divvy < ActiveRecord::Base
   require 'uri'
   require "net/http"
-
   require 'json'
+
+  scope :radius, ->(lat, lng, distance) {where("lat < ? AND lng < ? AND lat > ? AND lng > ? ", lat+distance, lng + distance,lat-distance, lng - distance)}
+
+  def self.find_stations(lat, lng)
+    distance = 0.0005
+    count = 0
+    while Divvy.radius(lat, lng, distance).count < 4
+      distance += 0.0005*Math.exp(count)
+      count += 1
+    end
+    Divvy.radius(lat, lng, distance)
+  end
+
+  def self.closest_station(lat,lng)
+    stations = Divvy.find_stations(lat,lng)
+    lat_dif = (stations.first.lat - lat).abs
+    lng_dif = (stations.first.lng - lng).abs
+    closest_station = stations.first
+    stations.each do |station|
+      if (station.lat - lat).abs <= lat_dif && (station.lng - lng).abs <= lng_dif
+        lat_dif = (station.lat - lat).abs
+        lng_dif = (station.lng - lng).abs
+        closest_station = station
+      end
+    end
+    closest_station
+  end
 
   def self.build_uri
     host = 'www.divvybikes.com'
