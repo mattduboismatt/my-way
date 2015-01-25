88c56e4c1e87716de89ba1da1bb65465d85f9e09
Armando Bulnes <armandogbulnesjr@gmail.com>
2015-01-24 22:53:08 -0600

    maybe this works?

---
+++ app/lib/algorithms/dollars.rb
@@ -0,0 +1,63 @@
+require './app/lib/modules/parking_whiz/parking.rb'
+
+class Route
+  attr_reader :travel_mode, :distance, :origin, :destination, :duration, :high_estimate, :low_estimate
+  def initialize(args)
+    @travel_mode = args[:travel_mode]
+    @distance = args[:distance]
+    @duration = args[:duration]
+    @origin = args[:origin]
+    @destination = args[:destination]
+    @high_estimate = args[:high_estimate]
+    @low_estimate = args[:low_estimate]
+  end
+end
+
+module DollarsAlgorithm
+
+  def self.run(route)
+    mode = route.travel_mode
+    distance = route.distance.to_f / 1600
+    duration = route.duration
+    actual_cost = 0.to_f
+
+    case mode
+      when 'driving'
+        park = Parking.new(route)
+        irs = distance * 0.56
+        park_cost = park.calculate_cost
+        puts "parking cost #{park_cost}"
+        actual_cost = park_cost + irs
+      when 'subway'
+        actual_cost = 2.25
+      when 'bus'
+        actual_cost = 2.00
+      when 'uber'
+        actual_cost = (route.high_estimate + route.low_estimate) / 2
+      when 'divvy'
+        actual_cost = 7.00
+      when 'cab'
+        base_fare = 3.25
+        mile_exp = distance * 1.80
+        time_exp = (duration/36) * 0.2
+        actual_cost = base_fare + mile_exp + time_exp
+    end
+    actual_cost = actual_cost.round(2)
+    dollars_exp = (100 - (actual_cost * 5)).to_i
+    puts "#{route.travel_mode} - $#{actual_cost} - #{dollars_exp} exp"
+  end
+end
+
+a = {'lat' => 41.783587, 'lng' => -87.77558}
+b = {'lat' => 41.908803, 'lng' =>  -87.679598}
+distance = 13600
+duration = 1800
+high = 27
+low = 23
+routes = []
+modes = %w(walking bicycling driving subway bus uber divvy cab)
+modes.each do |m|
+  routes << Route.new(origin: a, destination: b, distance: distance, duration: duration, travel_mode: m, low_estimate: low, high_estimate: high)
+end
+routes.each{ |r| DollarsAlgorithm.run(r) }
+
--- app/lib/modules/parking_whiz/parking.rb
+++ app/lib/modules/parking_whiz/parking.rb
@@ -8,9 +8,9 @@ class Parking
   attr_accessor :cost
   attr_reader  :lat, :long, :locations
 
-  def initialize(lat, long)
-    @lat = lat.to_f
-    @long = long.to_f
+  def initialize(route)
+    @lat = route.origin.lat.to_f
+    @long = route.desination.lng.to_f
     @locations = Array(Parkwhiz.search({ destination: coordinates_to_address}))
     @cost = 0
   end
@@ -23,6 +23,22 @@ class Parking
   def calculate_cost
     @locations.each {|location| @cost += location.price }
     @cost= @cost/@locations.length
+    if @cost == 0
+      @cost = Parking.add_street_parking
+    end
+    @cost
   end
+
+  def self.add_street_parking
+    if @lat >= 41.91077656 || @lat <= 41.8683927 ||@lng <= - 87.6481092
+     @cost =2
+   elsif @lat >= 41.8737774 && @lng >= -87.6356852 && @lat <= 41.885717
+     @cost = 6
+    else
+      @cost = 4
+   end
+   @cost
+  end
+
 
 end
