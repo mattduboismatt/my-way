78531b0e62ae77d164318bfe17855ce37ca0c27b
Bane Srdjevic <banesrdjevic@gmail.com>
2015-01-25 22:35:55 -0600

    added model tests for location, route, and trip

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Location' do
+  it 'location should belong to user' do
+    location = Location.new
+    user = User.new
+    user.locations << location
+    expect(location.user).to be user
+  end
+
+  it 'trip.origin should be a location' do
+    trip = Trip.new
+    user = User.new
+    trip.set_origin("apple", user)
+    expect(trip.origin).to be_a Location
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Trip, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Trip' do
+
+  it 'should belong to user' do
+    trip = Trip.new
+    user = User.new
+    user.trips << trip
+    expect(trip.user).to be user
+  end
+
+  it 'trip.origin should be a location' do
+    trip = Trip.new
+    user = User.new
+    trip.set_origin("apple", user)
+    expect(trip.origin).to be_a Location
+  end
+
+  it 'trip.destination should be a location' do
+    trip = Trip.new
+    user = User.new
+    trip.set_destination("apple", user)
+    expect(trip.destination).to be_a Location
+  end
+
+  it 'trip.destination should be a location' do
+    trip = Trip.new
+    user = User.new
+    trip.set_destination("apple", user)
+    expect(trip.destination.address).to eq("apple")
+  end
 end
