ab57e2d26fdaa6b1f0977c2f20ba01e93203cb32
Armando Bulnes <armandogbulnesjr@gmail.com>
2015-01-26 17:20:32 -0600

    adding limiting 1 per type helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def method_limiter(routes)
+    display_routes = []
+    display_routes << routes[0]
+    routes.each do |route|
+        if display_routes.any?{|r| r.travel_mode != route.travel_mode}
+          display_routes << route
+        end
+    end
+    display_routes
+  end
 end
