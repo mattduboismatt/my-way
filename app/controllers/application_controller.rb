aba6cd2b08fc3b353334e73596f12ac312b869a0
Armando Bulnes <armandogbulnesjr@gmail.com>
2015-01-26 13:25:36 -0600

    more touches on general design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,10 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
   helper_method :logged_in?
+
+  def weather
+    climate = Forecast.new(lat: 41.889685, lng: -87.637750)
+    climate.current_precise_temp
+  end
+  helper_method :weather
 end
