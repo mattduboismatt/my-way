b906ba6087c48354f7ea31bea7766c629dd276bd
Nirag Amin <nirag.amin@gmail.com>
2015-01-25 19:39:15 -0600

    adding fix to duration alg

--- app/lib/algorithms/duration.rb
+++ app/lib/algorithms/duration.rb
@@ -12,23 +12,27 @@ module DurationAlgorithm
 
   def self.run(route)
     mode = route.travel_mode
-    duration = route.duration ### in meters
+    duration = route.duration ### in seconds
     duration_exp = 0
     case mode
     when 'walking'
       duration_exp = ((-1.0/30)* (duration) + 100).to_i
     when 'driving'
-      duration_exp = ((100.0/3600**2)*(duration)**2).to_i
+      duration_exp = (4.224E-15*duration**5 - 4.169E-11*duration**4 + 1.398E-7*duration**3 - 1.771E-4*duration**2 + 0.084*duration+ 18.634).to_i
+      duration_exp < 100 ? duration_exp : 100
     when 'bicycling'
       duration_exp = ((-1.0/45)* (duration) + 100).to_i
     when 'bus'
       duration_exp = ((-0.000000005509)*(duration)**3 + (0.00003556)*(duration)**2 - (0.072)*(duration) + 100).to_i
-    when 'train'
+    when 'subway'
       duration_exp = ((-1.0/55)* (duration) + 100).to_i
     when 'uber'
-      duration_exp = ((100.0/3600**2)*(duration)**2).to_i
-    when 'taxi'
-      duration_exp = ((100.0/3600**2)*(duration)**2).to_i
+      duration += route.wait_time
+      duration_exp = (4.224E-15*duration**5 - 4.169E-11*duration**4 + 1.398E-7*duration**3 - 1.771E-4*duration**2 + 0.084*duration+ 18.634).to_i
+      duration_exp < 100 ? duration_exp : 100
+    when 'cab'
+      duration_exp = (4.224E-15*duration**5 - 4.169E-11*duration**4 + 1.398E-7*duration**3 - 1.771E-4*duration**2 + 0.084*duration+ 18.634).to_i
+      duration_exp < 100 ? duration_exp : 100
     end
     # puts "#{duration} - #{duration_exp}"
     duration_exp > 0 ? duration_exp : 0
