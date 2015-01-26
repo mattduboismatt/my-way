c1c5ae57e7afb5cf656dbf210cc296bffba8b113
Nirag Amin <nirag.amin@gmail.com>
2015-01-26 14:26:15 -0600

    Adding route merge fixes

--- app/algorithms/dollars.rb
+++ app/algorithms/dollars.rb
@@ -6,32 +6,53 @@ module DollarsAlgorithm
     actual_cost = 0.to_f
 
     case mode
-      when 'driving'
-        irs_cost = distance * 0.56
-        parking = Parking.new(route)
-        park_cost = parking.street_parking_cost
-        puts "parking cost #{park_cost}"
-        actual_cost = park_cost + irs_cost
-      when 'subway'
-        actual_cost = 2.25
-      when 'bus'
-        actual_cost = 2.00
-      when 'uber'
-        actual_cost = (route.high_estimate + route.low_estimate) / 2
-      when 'divvy'
-        actual_cost = 7.00
-      when 'cab'
-        base_cost = 3.25
-        mile_cost = distance * 1.80
-        time_cost = (duration/36) * 0.2
-        actual_cost = base_cost + mile_cost + time_cost
+    when 'driving'
+      irs_cost = distance * 0.56
+      parking = Parking.new(route)
+      park_cost = parking.street_parking_cost
+      puts "parking cost #{park_cost}"
+      actual_cost = park_cost + irs_cost
+    when 'subway'
+      actual_cost = 2.25
+    when 'bus'
+      actual_cost = 2.00
+    when 'uber'
+      actual_cost = (route.high_estimate + route.low_estimate) / 2
+    when 'divvy'
+      actual_cost = 7.00
+    when 'cab'
+      base_cost = 3.25
+      mile_cost = distance * 1.80
+      time_cost = (duration/36) * 0.2
+      actual_cost = base_cost + mile_cost + time_cost
     end
     actual_cost = actual_cost.round(2)
     actual_cost
   end
 
-  def self.run(actual_cost)
+  def self.run(actual_cost, route)
     dollars_exp = (100 - (actual_cost * 5)).to_i
+
+    case mode
+    when 'driving'
+      duration = route.duration
+      dollars_exp = (100 - (actual_cost * 5)).to_i
+    when 'subway'
+      duration = route.duration
+      dollars_exp = (100 - (actual_cost * 5)).to_i
+    when 'bus'
+      duration = route.duration
+      dollars_exp = (100 - (actual_cost * 5)).to_i
+    when 'uber'
+      duration = route.duration + route.wait_time
+      dollars_exp = (100 - (actual_cost * 5)).to_i
+    when 'divvy'
+      duration = route.duration +
+      dollars_exp = (100 - (actual_cost * 5)).to_i
+    when 'cab'
+      duration = route.duration + route.wait_time
+      dollars_exp = (100 - (actual_cost * 5)).to_i
+    end
     dollars_exp > 0 ? dollars_exp : 0
   end
 end
