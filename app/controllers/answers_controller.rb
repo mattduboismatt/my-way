07b0e2af70f039f3081d2697b3ae124b24423e4f
Matt DuBois <mattdubois2@gmail.com>
2015-01-26 19:36:12 -0600

    controllers work for answering 7 questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,6 +3,11 @@ class AnswersController < ApplicationController
   def create
     answer = Answer.find_by_content(params['commit'])
     UsersAnswer.create!(user: current_user, answer: answer)
+    if answer.question.id < 7
+      redirect_to user_question_path(user_id: current_user.id, id: answer.question.id + 1)
+    else
+      redirect_to root_path
+    end
   end
 
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 
-  def index
-    @questions = Question.all
+  def show
+    @question = Question.find(params[:id])
   end
 
 end
