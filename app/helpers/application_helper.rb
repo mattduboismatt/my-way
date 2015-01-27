f2c3affd7fe4d11b69b84a3e432318ced16efd6f
Armando Bulnes <armandogbulnesjr@gmail.com>
2015-01-26 22:07:02 -0600

    refactor of uniq route helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,6 @@
 module ApplicationHelper
   def method_limiter(routes)
-    display_routes = []
-    display_routes << routes[0]
-    routes.each do |route|
-      if display_routes.any?{|r| r.travel_mode != route.travel_mode}
-        display_routes << route
-      end
-    end
+    display_routes =  routes.uniq{|r| r.travel_mode}
     display_routes
   end
 
