ace213d7824d83c77d0b8957322ce86989446938
Matt DuBois <mattdubois2@gmail.com>
2015-01-27 15:09:29 -0600

    call update_multipliers whenever load root_path

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,9 @@
 class TripsController < ApplicationController
   def create
-    @trip = Trip.create!(user: current_user)
+    @trip = Trip.create(user: current_user)
     @trip.set_origin(params[:origin], current_user)
     @trip.set_destination(params[:destination], current_user)
-    @routes = @trip.generate_and_score_routes
+    @routes = @trip.generate_and_score_routes(current_user)
     render :show
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      current_user.update_multipliers
+    end
   end
 end
