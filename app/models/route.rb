a34ab32658072df8dc92c47af5c740df1caa44f2
Matt DuBois <mattdubois2@gmail.com>
2015-01-27 13:18:47 -0600

    add in traffic multiplier for vehicle modes AND changed total_exp to weighted_exp

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,16 +1,16 @@
 class Route
   attr_reader :travel_mode
-  attr_accessor :distance_exp, :duration_exp, :dollars_exp, :weather_exp, :safety_exp, :comfort_exp, :total_exp, :actual_cost
+  attr_accessor :distance_exp, :duration, :dollars_exp, :weather_exp, :safety_exp, :comfort_exp, :weighted_exp, :actual_cost
   def initialize(args)
     @travel_mode = args[:travel_mode]
     @distance_exp = 0
-    @duration_exp = 0
     @dollars_exp = 0
     @weather_exp = 0
     @safety_exp = 0
     @comfort_exp = 0
-    @total_exp = 0
+    @weighted_exp = 0
     @actual_cost = 0.to_f
+    @duration = 0
   end
 
   def calculate_and_set_all_exp(route_data, forecast)
@@ -19,42 +19,69 @@ class Route
     @dollars_exp = DollarsAlgorithm.run(route_data, @actual_cost)
     @weather_exp = WeatherAlgorithm.run(route_data, forecast)
     @safety_exp = SafetyAlgorithm.run(route_data.travel_mode)
-    self.set_total_exp
+    self.set_weighted_exp
   end
 
-  def set_total_exp
-    @total_exp = @distance_exp + @duration_exp + @dollars_exp + @weather_exp + @safety_exp + @comfort_exp
+  def set_weighted_exp
+    @weighted_exp = @distance_exp + @dollars_exp + @weather_exp + @safety_exp
   end
 
   def self.google_driving(route, forecast)
     uber_res = UberParser.run(route)
-    [regular(route, forecast), uber(uber_res, forecast), cab(route, uber_res, forecast)]
+    [driving(route, forecast), uber(uber_res, forecast), cab(route, uber_res, forecast)]
   end
 
   def self.google_bicycling(route, forecast)
-    [regular(route, forecast), divvy(route, forecast)]
+    [bicycling(route, forecast), divvy(route, forecast)]
   end
 
-  def self.google_transit_and_walking(route, forecast)
-    regular(route, forecast)
+  def self.driving(route, forecast)
+    driving_route = Route.new(travel_mode: route.travel_mode)
+    driving_route.duration = route.duration*traffic_multiplier
+    driving_route.calculate_and_set_all_exp(route, forecast)
+    driving_route
   end
 
-  def self.regular(route, forecast)
-    reg_route = Route.new(travel_mode: route.travel_mode)
-    reg_route.calculate_and_set_all_exp(route, forecast)
-    reg_route
+  def self.walking(route, forecast)
+    walking_route = Route.new(travel_mode: route.travel_mode)
+    walking_route.duration = route.duration
+    walking_route.calculate_and_set_all_exp(route, forecast)
+    walking_route
+  end
+
+  def self.subway(route, forecast)
+    subway_route = Route.new(travel_mode: route.travel_mode)
+    subway_route.duration = route.duration #delays?
+    subway_route.calculate_and_set_all_exp(route, forecast)
+    subway_route
+  end
+
+  def self.bus(route, forecast)
+    bus_route = Route.new(travel_mode: route.travel_mode)
+    bus_route.duration = route.duration*traffic_multiplier
+    bus_route.calculate_and_set_all_exp(route, forecast)
+    bus_route
+  end
+
+  def self.bicycling(route, forecast)
+    bicycling_route = Route.new(travel_mode: route.travel_mode)
+    bicycling_route.duration = route.duration
+    bicycling_route.calculate_and_set_all_exp(route, forecast)
+    bicycling_route
   end
 
   def self.uber(uber_res, forecast)
     uber_route = Route.new(travel_mode: 'uber')
+    uber_route.duration = uber_res.wait_time + uber_res.duration*traffic_multiplier
     uber_route.calculate_and_set_all_exp(uber_res, forecast)
     uber_route
   end
 
   def self.cab(route, uber_res, forecast)
     cab_route = Route.new(travel_mode: 'cab')
+    cab_route.duration = uber_res.wait_time + route.duration*traffic_multiplier
     route.travel_mode = 'cab'
-    route.wait_time = (uber_res.wait_time)*2
+    route.wait_time = uber_res.wait_time
     cab_route.calculate_and_set_all_exp(route, forecast)
     cab_route
   end
@@ -62,8 +89,21 @@ class Route
   def self.divvy(route, forecast)
     divvy_route = Route.new(travel_mode: 'divvy')
     route.travel_mode = 'divvy'
+    divvy_route.duration = route.duration #add in divvy find time?
     divvy_route.calculate_and_set_all_exp(route, forecast)
     divvy_route
+  end
+
+  def self.traffic_multiplier
+    hour = Time.now.hour
+    traffic_multiplier = 1
+    case hour
+    when 6, 16, 19
+      traffic_multiplier = 1.1
+    when 7, 8, 17, 18
+      traffic_multiplier = 1.2
+    end
+    traffic_multiplier
   end
 
 end
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -10,10 +10,6 @@ describe 'Route' do
     expect(route.distance_exp).to eq(0)
   end
 
-  it 'route duration_exp should be 0' do
-    expect(route.duration_exp).to eq(0)
-  end
-
   it 'route dollars_exp should be 0' do
     expect(route.dollars_exp).to eq(0)
   end
@@ -30,16 +26,20 @@ describe 'Route' do
     expect(route.comfort_exp).to eq(0)
   end
 
-  it 'route total_exp should be 0' do
-    expect(route.total_exp).to eq(0)
+  it 'route weighted_exp should be 0' do
+    expect(route.weighted_exp).to eq(0)
   end
 
   it 'route actual_cost should be 0.0' do
     expect(route.actual_cost).to eq(0.0)
   end
 
-  it 'set_total_exp should be 0.0' do
-    route.set_total_exp
+  it 'route duration should be 0.0' do
+    expect(route.duration).to eq(0.0)
+  end
+
+  it 'set_weighted_exp should be 0.0' do
+    route.set_weighted_exp
     expect(route.actual_cost).to eq(0.0)
   end
 
