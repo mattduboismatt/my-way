4fe7598ac133eb12ef93a6e861ca8af9956322c5
Nirag Amin <nirag.amin@gmail.com>
2015-01-27 15:03:36 -0600

    adding goole map app test url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,22 @@ module ApplicationHelper
     end
   end
 
+  def app_link(trip,route)
+    # case r.travel_mode
+    # when "bus"
+    #   "fa fa-bus"
+    # when "subway"
+    #   "fa fa-train"
+    # when "walking"
+    #   "fa fa-male"
+    # when "driving", "uber"
+    #   "fa fa-car"
+    # when 'bicycling', 'divvy'
+    #   "fa fa-bicycle"
+    # when "cab"
+    #   "fa fa-taxi"
+    # end
+    "comgooglemaps://?saddr=#{trip.origin.address}&daddr#{trip.destination.address}&directionsmode:driving"
+  end
+
 end
