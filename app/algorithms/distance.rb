ea7f26fa2d323cbf95295f8ee00208e24bb7d672
Nirag Amin <nirag.amin@gmail.com>
2015-01-28 10:22:48 -0600

    adding bus equation

--- app/algorithms/distance.rb
+++ app/algorithms/distance.rb
@@ -15,10 +15,8 @@ module DistanceAlgorithm
       distance_exp = ( 0.688*(distance/1600)**3 - 4.322*(distance/1600)**2 - 6.515*(distance/1600) + 100).to_i
     when 'subway'
       distance_exp = (0.423*(distance/1600)**3 - 1.177*(distance/1600)**2 - 18.834*(distance/1600) + 100).to_i
-    when 'subway'
-
-distance_exp = (0.416*(distance/1600)**3 - 1.114*(distance/1600)**2 - 19.011*(distance/1600) + 100).to_i
-
+    when 'bus'
+      distance_exp = (0.416*(distance/1600)**3 - 1.114*(distance/1600)**2 - 19.011*(distance/1600) + 100).to_i
     end
     distance_exp > 0 ? distance_exp : 0
   end
