215f775a0b507cc0d7ca4cd4a94e31d9fa448709
Matt DuBois <mattdubois2@gmail.com>
2015-01-27 20:33:11 -0600

    whitelist works for show page now

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,14 @@
 class TripsController < ApplicationController
   def create
+    wl = params['user']['whitelist']
     if current_user
       current_user.update_multipliers
-      current_user.update_whitelist(params['user']['whitelist'])
+      current_user.update_whitelist(wl)
     end
     @trip = Trip.create(user: current_user)
     @trip.set_origin(params[:origin], current_user)
     @trip.set_destination(params[:destination], current_user)
-    @routes = @trip.generate_and_score_routes(current_user)
+    @routes = @trip.generate_and_score_routes(current_user, wl)
     render :show
   end
 end
