96a2e591d3cc94d7369b71c74044a6d4a63d5d77
Nirag Amin <nirag.amin@gmail.com>
2015-01-27 18:39:01 -0600

    adding address formater

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,13 @@
 class Location < ActiveRecord::Base
   belongs_to :user
   belongs_to :trip
+  after_initialize :format_address
+
+  def format_address
+    address = self.address.downcase
+    if !address.include?("chicago")
+      address = address + " chicago il"
+    end
+    self.address = address.gsub(",", " ")
+  end
 end
