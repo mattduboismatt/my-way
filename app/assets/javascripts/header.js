c6ec00c04ccc4c279077bb315af3f73af4622320
Matt DuBois <mattdubois2@gmail.com>
2015-01-29 12:31:17 -0600

    formula popup works!

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
 
+  var popUp = $('#formula-pop-up')
+  var siteContent = $(".site-content");
+
   $('#formula').on('click', function(e){
     e.preventDefault();
-    $('#formula-pop-up').removeClass('hide-me')
-    // $('#formula-pop-up').css('visibility','visible');
+    popUp.show();
+    popUp.animate({opacity: 1}, 400);
+    siteContent.animate({opacity: 0.3}, 400);
   });
+
+  $('#close-pop-up').on('click', function(e) {
+    e.preventDefault();
+    popUp.animate({opacity: 0}, 400, 'swing', function(e) { $(this).hide();});
+    siteContent.animate({opacity: 1}, 400);
+  });
+
 });
