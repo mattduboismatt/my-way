589c7785c6f5e52318964e84d07937666c62af6e
Matt DuBois <mattdubois2@gmail.com>
2015-01-29 20:14:26 -0600

    more seed encouragements

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -11,6 +11,6 @@ module QuestionHelper
 
 
   def encouragement
-    ['Keep Going!', "You're almost there!", "Just a few more!", "For Freedom!", "Some days you’re the pigeon; some days you’re the statue."].sample
+    ['Keep Going!', "You're almost there!", "Just a few more!", "For Freedom!", "Some days you’re the pigeon; some days you’re the statue.", "Don't start no. Won't be no.", "There is always a light at the end of the tunnel. Sometimes it is a train."].sample
   end
 end
